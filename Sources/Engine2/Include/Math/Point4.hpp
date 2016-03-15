4f3e2966f1878d11ac9de97680a7f72579b8ac2e
Grégory COMPTE <chibiketan@gmail.com>
2016-03-15 16:02:13 +0100

    Début de l'implémentation basique d'une classe Point4

--- Sources/Engine2/Include/Math/Point4.hpp
+++ Sources/Engine2/Include/Math/Point4.hpp
@@ -2,7 +2,27 @@
 // Created by Gregory_Compte on 15/12/2015.
 //
 
-#ifndef CROISSANT_POINT4_HPP
-#define CROISSANT_POINT4_HPP
+#ifndef CROISSANT_ENGINE_MATH_POINT4_HPP
+#  define CROISSANT_ENGINE_MATH_POINT4_HPP
 
-#endif //CROISSANT_POINT4_HPP
+#  include "Engine.hpp"
+#  include <array>
+
+namespace Croissant
+{
+	namespace Math
+	{
+		class ENGINE_API Point4
+		{
+		public:
+			using type = float;
+			using type_ref = type&;
+			using type_cref = type const&;
+
+		private:
+			std::array<type, 4>	m_elements;
+		};
+	}
+}
+
+#endif //CROISSANT_ENGINE_MATH_POINT4_HPP
