47b2a6b0e8607a0727ca90973338f19984a4b9e9
Grégory Compte <chibiketan@gmail.com>
2022-07-05 23:57:34 +0200

    - Création de la Swapchain \o/

--- vulkan/main.cpp
+++ vulkan/main.cpp
@@ -11,6 +11,7 @@
 #include <functional>
 #include <optional>
 #include <volk.h>
+#include <limits>
 
 struct GlfwWindowDestructor {
     void operator()(GLFWwindow* window) {
@@ -34,6 +35,15 @@ struct VulkanDeviceDestructor {
     }
 };
 
+struct VulkanSwapchainDestructor {
+    void operator()(VkSwapchainKHR* swapchain) {
+        if (*swapchain != nullptr) {
+            // Comment récupérer le device ???
+            vkDestroySwapchainKHR(nullptr, *swapchain, nullptr);
+        }
+    }
+};
+
 class TestVulkan
 {
 public:
@@ -46,6 +56,7 @@ public:
         , m_surface{ new VkSurfaceKHR{nullptr}, [&](VkSurfaceKHR* item) { this->releaseSurface(*item); }}
         , m_physicalDevice{nullptr}
         , m_device{new VkDevice(nullptr)}
+        , m_swapChain{ new VkSwapchainKHR{nullptr}, [&](VkSwapchainKHR* item) { this->releaseSwapchain(*item); }}
         , m_graphicsQueue{nullptr}
         , m_presentQueue{nullptr}
     {
@@ -64,6 +75,7 @@ public:
         this->createSurface();
         this->pickPhysicalDevice();
         this->createLogicalDevice();
+        this->createSwapChain();
     }
 
     void run()
@@ -84,6 +96,7 @@ private:
     std::unique_ptr<VkSurfaceKHR, std::function<void (VkSurfaceKHR*)>> m_surface;
     VkPhysicalDevice m_physicalDevice;
     std::unique_ptr<VkDevice, VulkanDeviceDestructor> m_device;
+    std::unique_ptr<VkSwapchainKHR, std::function<void (VkSwapchainKHR*)>> m_swapChain;
     VkQueue m_graphicsQueue;
     VkQueue m_presentQueue;
 #ifdef CROISSANT_VULKAN_VALIDATION_LAYER
@@ -249,6 +262,14 @@ private:
         vkDestroySurfaceKHR(*this->m_instanceVulkan, surface, nullptr);
     }
 
+    void releaseSwapchain(VkSwapchainKHR swapchain) {
+        if (nullptr == swapchain) {
+            return;
+        }
+
+        vkDestroySwapchainKHR(*this->m_device, swapchain, nullptr);
+    }
+
 
     bool isDeviceSuitable(VkPhysicalDevice device) {
         VkPhysicalDeviceProperties deviceProperties{};
@@ -367,6 +388,73 @@ private:
             return availableFormats[0];
         }
         return *result;
+    }
+
+    VkPresentModeKHR chooseSwapPresentMode(std::vector<VkPresentModeKHR> const& availablePresentModes) {
+        for (auto const& availablePresentMode : availablePresentModes) {
+            if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
+                return availablePresentMode;
+            }
+        }
+
+        // toujours présent d'après la spec
+        return VK_PRESENT_MODE_FIFO_KHR;
+    }
+
+    VkExtent2D chooseSwapExtent(VkSurfaceCapabilitiesKHR const& capabilities) {
+        if (capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max()) {
+            return capabilities.currentExtent;
+        }
+
+        VkExtent2D actualExtent = { static_cast<uint32_t>(m_width), static_cast<uint32_t>(m_height) };
+
+        actualExtent.width = std::clamp(actualExtent.width, capabilities.minImageExtent.width, capabilities.maxImageExtent.width);
+        actualExtent.height = std::clamp(actualExtent.height, capabilities.minImageExtent.height, capabilities.maxImageExtent.height);
+
+        return actualExtent;
+    }
+
+    void createSwapChain() {
+        auto swapChainSupport = querySwapChainSupport(m_physicalDevice);
+
+        auto surfaceFormat = chooseSwapSurfaceFormat(swapChainSupport.formats);
+        auto presentMode = chooseSwapPresentMode(swapChainSupport.presentModes);
+        auto extent = chooseSwapExtent(swapChainSupport.capabilities);
+        auto imageCount = swapChainSupport.capabilities.minImageCount + 1;
+
+        if (swapChainSupport.capabilities.maxImageCount > 0 && imageCount > swapChainSupport.capabilities.maxImageCount) {
+            imageCount = swapChainSupport.capabilities.maxImageCount;
+        }
+
+        auto indices = findQueueFamilies(m_physicalDevice);
+        uint32_t queueFamilyIndices[] = {indices.graphicsFamily.value(), indices.presentFamily.value()};
+
+        VkSwapchainCreateInfoKHR createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+        createInfo.surface = *m_surface;
+        createInfo.minImageCount = imageCount;
+        createInfo.imageFormat = surfaceFormat.format;
+        createInfo.imageColorSpace = surfaceFormat.colorSpace;
+        createInfo.imageExtent = extent;
+        createInfo.imageArrayLayers = 1;
+        createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+        if (indices.graphicsFamily != indices.presentFamily) {
+            createInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+            createInfo.queueFamilyIndexCount = 2;
+            createInfo.pQueueFamilyIndices = queueFamilyIndices;
+        } else {
+            createInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+            createInfo.queueFamilyIndexCount = 0; // Optionnel
+            createInfo.pQueueFamilyIndices = nullptr; // Optionnel
+        }
+        createInfo.preTransform = swapChainSupport.capabilities.currentTransform;
+        createInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+        createInfo.presentMode = presentMode;
+        createInfo.clipped = VK_TRUE;
+        createInfo.oldSwapchain = VK_NULL_HANDLE;
+        if (vkCreateSwapchainKHR(*this->m_device, &createInfo, nullptr, this->m_swapChain.get()) != VK_SUCCESS) {
+            throw std::runtime_error("Swapchain creation failed");
+        }
     }
 
     void pickPhysicalDevice() {
