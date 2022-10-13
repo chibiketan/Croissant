f3d625aa32de6c263b390e3615e30be28534b279
Grégory Compte <chibiketan@gmail.com>
2022-10-13 23:02:53 +0200

    - Ajout de 2 shaders dans des fichiers séparés - Compilation des shader lors de la compilation du code - Chargement des shaders - Création du pipeline de rendu

--- vulkan/main.cpp
+++ vulkan/main.cpp
@@ -12,6 +12,8 @@
 #include <optional>
 #include <volk.h>
 #include <limits>
+#include <fstream>
+#include <filesystem>
 
 struct GlfwWindowDestructor {
     void operator()(GLFWwindow* window) {
@@ -57,6 +59,7 @@ public:
         , m_swapChainExtent{}
         , m_swapChainImages{}
         , m_swapChainImageViews{}
+        , m_pipelineLayout{ new VkPipelineLayout {nullptr}, [&](VkPipelineLayout* item) { this->releasePipelineLayout(*item); }}
     {
 
     }
@@ -75,6 +78,7 @@ public:
         this->createLogicalDevice();
         this->createSwapChain();
         this->createImageViews();
+        this->createGraphicsPipeline();
     }
 
     void run()
@@ -102,11 +106,196 @@ private:
     VkExtent2D m_swapChainExtent;
     std::vector<VkImage> m_swapChainImages;
     std::vector<std::unique_ptr<VkImageView, std::function<void (VkImageView*)>>> m_swapChainImageViews;
+    std::unique_ptr<VkPipelineLayout, std::function<void (VkPipelineLayout*)>>  m_pipelineLayout;
 #ifdef CROISSANT_VULKAN_VALIDATION_LAYER
     constexpr static bool enableValidationLayer = true;
 #else
     constexpr static bool enableValidationLayer = false;
 #endif
+
+    static std::vector<char> readFile(const std::string& filename) {
+        std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+        if (!file.is_open()) {
+            throw std::runtime_error(std::string {"échec de l'ouverture du fichier "} + filename + "!");
+        }
+
+        size_t fileSize = (size_t) file.tellg();
+        std::vector<char> buffer(fileSize);
+        file.seekg(0);
+        file.read(buffer.data(), fileSize);
+        file.close();
+
+        return buffer;
+    }
+
+    void createGraphicsPipeline() {
+        auto vertShaderCode = readFile("vulkan/shaders/shader.vert.spv");
+        auto fragShaderCode = readFile("vulkan/shaders/shader.frag.spv");
+
+        std::unique_ptr<VkShaderModule, std::function<void (VkShaderModule*)>> vertShaderModule = createShaderModule(vertShaderCode);
+        std::unique_ptr<VkShaderModule, std::function<void (VkShaderModule*)>> fragShaderModule = createShaderModule(fragShaderCode);
+
+
+        VkPipelineShaderStageCreateInfo vertShaderStageInfo{};
+        vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+        vertShaderStageInfo.module = *vertShaderModule;
+        vertShaderStageInfo.pName = "main";
+
+
+        VkPipelineShaderStageCreateInfo fragShaderStageInfo{};
+        fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+        fragShaderStageInfo.module = *fragShaderModule;
+        fragShaderStageInfo.pName = "main";
+
+
+        VkPipelineShaderStageCreateInfo shaderStages[] = {vertShaderStageInfo, fragShaderStageInfo};
+
+
+        VkPipelineVertexInputStateCreateInfo vertexInputInfo{};
+        vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+        vertexInputInfo.vertexBindingDescriptionCount = 0;
+        vertexInputInfo.pVertexBindingDescriptions = nullptr; // Optionnel
+        vertexInputInfo.vertexAttributeDescriptionCount = 0;
+        vertexInputInfo.pVertexAttributeDescriptions = nullptr; // Optionnel
+
+
+        VkPipelineInputAssemblyStateCreateInfo inputAssembly{};
+        inputAssembly.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+        inputAssembly.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+        inputAssembly.primitiveRestartEnable = VK_FALSE;
+
+        VkViewport viewport{};
+        viewport.x = 0.0f;
+        viewport.y = 0.0f;
+        viewport.width = (float) this->m_swapChainExtent.width;
+        viewport.height = (float) this->m_swapChainExtent.height;
+        viewport.minDepth = 0.0f;
+        viewport.maxDepth = 1.0f;
+
+
+        VkRect2D scissor{};
+        scissor.offset = {0, 0};
+        scissor.extent = this->m_swapChainExtent;
+
+
+        VkPipelineViewportStateCreateInfo viewportState{};
+        viewportState.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+        viewportState.viewportCount = 1;
+        viewportState.pViewports = &viewport;
+        viewportState.scissorCount = 1;
+        viewportState.pScissors = &scissor;
+
+        VkPipelineRasterizationStateCreateInfo rasterizer{};
+        rasterizer.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+        rasterizer.depthClampEnable = VK_FALSE;
+        rasterizer.rasterizerDiscardEnable = VK_FALSE;
+        rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
+        rasterizer.lineWidth = 1.0f;
+        rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
+        rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
+        rasterizer.depthBiasEnable = VK_FALSE;
+        rasterizer.depthBiasConstantFactor = 0.0f; // Optionnel
+        rasterizer.depthBiasClamp = 0.0f; // Optionnel
+        rasterizer.depthBiasSlopeFactor = 0.0f; // Optionnel
+
+
+        VkPipelineMultisampleStateCreateInfo multisampling{};
+        multisampling.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+        multisampling.sampleShadingEnable = VK_FALSE;
+        multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+        multisampling.minSampleShading = 1.0f; // Optionnel
+        multisampling.pSampleMask = nullptr; // Optionnel
+        multisampling.alphaToCoverageEnable = VK_FALSE; // Optionnel
+        multisampling.alphaToOneEnable = VK_FALSE; // Optionnel
+
+
+        VkPipelineColorBlendAttachmentState colorBlendAttachment{};
+        colorBlendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+        colorBlendAttachment.blendEnable = VK_FALSE;
+        colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_ONE; // Optionnel
+        colorBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ZERO; // Optionnel
+        colorBlendAttachment.colorBlendOp = VK_BLEND_OP_ADD; // Optionnel
+        colorBlendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE; // Optionnel
+        colorBlendAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO; // Optionnel
+        colorBlendAttachment.alphaBlendOp = VK_BLEND_OP_ADD; // Optionnel
+        // Exemple avec du blend des couleurs
+        //colorBlendAttachment.blendEnable = VK_TRUE;
+        //colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA;
+        //colorBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;
+        //colorBlendAttachment.colorBlendOp = VK_BLEND_OP_ADD;
+        //colorBlendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;
+        //colorBlendAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO;
+        //colorBlendAttachment.alphaBlendOp = VK_BLEND_OP_ADD;
+
+
+        VkPipelineColorBlendStateCreateInfo colorBlending{};
+        colorBlending.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+        colorBlending.logicOpEnable = VK_FALSE;
+        colorBlending.logicOp = VK_LOGIC_OP_COPY; // Optionnel
+        colorBlending.attachmentCount = 1;
+        colorBlending.pAttachments = &colorBlendAttachment;
+        colorBlending.blendConstants[0] = 0.0f; // Optionnel
+        colorBlending.blendConstants[1] = 0.0f; // Optionnel
+        colorBlending.blendConstants[2] = 0.0f; // Optionnel
+        colorBlending.blendConstants[3] = 0.0f; // Optionnel
+
+
+        std::vector<VkDynamicState> dynamicStates = {
+                VK_DYNAMIC_STATE_VIEWPORT,
+                VK_DYNAMIC_STATE_LINE_WIDTH
+        };
+
+        VkPipelineDynamicStateCreateInfo dynamicState{};
+        dynamicState.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
+        dynamicState.dynamicStateCount = static_cast<uint32_t>(dynamicStates.size());
+        dynamicState.pDynamicStates = dynamicStates.data();
+
+        VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
+        pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+        pipelineLayoutInfo.setLayoutCount = 0;            // Optionnel
+        pipelineLayoutInfo.pSetLayouts = nullptr;         // Optionnel
+        pipelineLayoutInfo.pushConstantRangeCount = 0;    // Optionnel
+        pipelineLayoutInfo.pPushConstantRanges = nullptr; // Optionnel
+
+        if (vkCreatePipelineLayout(*this->m_device, &pipelineLayoutInfo, nullptr, this->m_pipelineLayout.get()) != VK_SUCCESS) {
+            throw std::runtime_error("échec de la création du pipeline layout!");
+        }
+    }
+
+    void releaseShaderModule(VkShaderModule shaderModule) {
+        if (nullptr == shaderModule) {
+            return;
+        }
+
+        std::cout << "Release shader module" << std::endl;
+        vkDestroyShaderModule(*this->m_device, shaderModule, nullptr);
+    }
+
+    void releasePipelineLayout(VkPipelineLayout pipelineLayout) {
+        if (nullptr == pipelineLayout) {
+            return;
+        }
+
+        std::cout << "Release pipeline layout" << std::endl;
+        vkDestroyPipelineLayout(*this->m_device, pipelineLayout, nullptr);
+    }
+
+    std::unique_ptr<VkShaderModule, std::function<void (VkShaderModule*)>> createShaderModule(const std::vector<char>& code) {
+        VkShaderModuleCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+        createInfo.codeSize = code.size();
+        createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+
+        std::unique_ptr<VkShaderModule, std::function<void (VkShaderModule*)>> shaderModule { new VkShaderModule{nullptr}, [&](VkShaderModule* item) { this->releaseShaderModule(*item); } };
+        if (vkCreateShaderModule(*this->m_device, &createInfo, nullptr, shaderModule.get()) != VK_SUCCESS) {
+            throw std::runtime_error("échec de la création d'un module shader!");
+        }
+
+        return shaderModule;
+    }
 
     std::vector<std::string> getInstanceLayers() {
         uint32_t layerCount;
--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,31 @@
+-- xmake f -c -p mingw --mingw=D:/logiciels/msys64/mingw64/ --cc=clang --cxx=clang++
+rule("shader.compile")
+    set_extensions(".frag", ".vert")
 
-add_requires("volk", "glfw =3.3.5")
+    on_build_file(function(target, sourcefile, opt)
+        import("core.project.depend")
+        import("utils.progress") -- it only for v2.5.9, we need use print to show progress below v2.5.8
+
+        -- replace .md with .html
+        local targetfile = path.join(target:targetdir(),sourcefile .. ".spv")
+
+        -- make sure build directory exists
+        os.mkdir(path.directory(targetfile))
+
+
+
+        -- only rebuild the file if its changed since last run
+        depend.on_changed(function ()
+            os.vrunv('glslangValidator', { '-V', '-o', targetfile, sourcefile })
+            progress.show(opt.progress, "${color.build.object}compile shader %s", sourcefile)
+        end, {files = sourcefile})
+    end)
+rule_end()
+
+
+add_requires("volk", "glfw =3.3.5", "glslang")
 set_languages("cxx20")
+set_warnings("allextra")
 
 target("engine")
     set_kind("static")
@@ -25,7 +50,18 @@ target("vulkan_test")
     set_kind("binary")
     add_files("vulkan/**.cpp")
     add_deps("engine")
-    add_packages("glfw", "volk")
+    add_packages("glfw", "volk", "glslang")
+    add_rules("shader.compile")
+    for _, filepath in pairs(os.files("vulkan/**.frag")) do
+        -- compile shaders
+        -- add_files(filepath, {rule = "nzsl.compile.shaders"})
+        add_files(filepath)
+    end
+    for _, filepath in pairs(os.files("vulkan/**.vert")) do
+        -- compile shaders
+        -- add_files(filepath, {rule = "nzsl.compile.shaders"})
+        add_files(filepath)
+    end
 
 target("get_config")
     set_kind("binary")
